38ffae3e844ab14f15f53e97ff57cf5972f71248
makethumb <sourabhs701@gmail.com>
2025-08-28 16:51:45 +0530

    react: state_management

--- react_foundation/src/App.jsx
+++ react_foundation/src/App.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
 const App = () => {
-  return (<></>)
+  const [Likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(Likes + 1)
+  }
+
+  return (
+    <div className="bg-zinc-900 h-screen flex justify-center items-center">
+
+      <button onClick={handleClick} className="bg-zinc-800 text-zinc-200 p-4 rounded-3xl">Likes: {Likes}</button>
+    </div>
+  )
 };
 
 
