b6318729f8093f4efacbfbe1485a2f84050a2184
makethumb <sourabhs701@gmail.com>
2025-08-28 17:52:02 +0530

    react: todo_basic

--- react_foundation/README.md
+++ react_foundation/README.md
@@ -1,3 +1,9 @@
 # React 101
 
 this particular repository is created to Learn React from nextjs/learn
+
+## since we have hands on knowledege on states in react lets try to build a simple todo application
+
+1. show/display area (where user can see, update, remove, markDone there todos)
+2. input area and add todos
+
--- react_foundation/src/App.jsx
+++ react_foundation/src/App.jsx
@@ -1,16 +1,48 @@
 import { useState } from "react";
 const App = () => {
-  const [Likes, setLikes] = useState(0);
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
 
-  function handleClick() {
-    setLikes(Likes + 1)
-  }
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (input.trim()) {
+      setTodos([...todos, { text: input, done: false }]);
+      setInput('');
+    }
+  };
+
+  const deleteTodo = (idx) => {
+    setTodos(todos.filter((_, i) => i !== idx));
+  };
+
 
   return (
-    <div className="bg-zinc-900 h-screen flex justify-center items-center">
+    <div className=" h-screen flex justify-center items-center">
+      <div className="flex flex-col border-x-1 p-4">
 
-      <button onClick={handleClick} className="bg-zinc-800 text-zinc-200 p-4 rounded-3xl">Likes: {Likes}</button>
-    </div>
+        <span className="text-3xl mb-4 pb-2 flex justify-center items-center border-b-1  border-gray-300">Todo App</span>
+        {todos && todos.length > 0 ? (<ul className="flex flex-col gap-2">
+          {todos.map((todo, idx) => (
+            <div className="flex justify-between border-b-1  border-gray-300">
+              <li key={idx} className=" p-1 m-2">
+                {`${idx + 1} : ${todo.text}`}
+              </li>
+              <button onClick={() => deleteTodo(idx)} className="text-white rounded-xl px-2 my-2 bg-gray-200 text-l">🗑️</button>
+            </div>
+          ))}
+        </ul>) : (<span className="flex justify-center items-center text-gray-300">No Todo Found </span>)}
+
+        <form onSubmit={addTodo} className=" flex gap-2 mt-4">
+          <input value={input}
+            onChange={e => setInput(e.target.value)}
+            placeholder="Add a todo"
+            className="p-2 border-1 rounded-xl"
+          ></input>
+          <button type="submit" className="bg-zinc-800 text-zinc-200 p-2 rounded-xl">Add</button>
+        </form>
+
+      </div>
+    </div >
   )
 };
 
